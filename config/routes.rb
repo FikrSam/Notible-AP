09b24ab5b58498cb18c1d3042a26030c372a3122
Fikir <fikirmrw@gmail.com>
2025-07-23 19:54:23 +0300

    Configure API routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
+# config/routes.rb
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Authentication Routes
+  post 'auth/signup', to: 'authentication#signup'
+  post 'auth/login', to: 'authentication#login'
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # Custom route to get the username
+  get 'notes/username', to: 'notes#get_username'
 
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # Notes Routes (CRUD operations)
+  resources :notes
 end
