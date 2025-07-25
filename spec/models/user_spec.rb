7cd620f85b72d004f3ad2804a8c400a4aba764d1
Fikir <fikirmrw@gmail.com>
2025-07-25 10:19:03 +0300

    Add request specs for notes and authentication APIs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,11 +7,17 @@ RSpec.describe User, type: :model do
 
   # Test validations
   it { should validate_presence_of(:username) }
-  it { should validate_uniqueness_of(:username) }
+  # The fix is on this line: we add .case_insensitive to match our validation behavior
+  it { should validate_uniqueness_of(:username).case_insensitive }
+
+  # Provide a valid user instance for the uniqueness matcher to avoid the "cannot be null" error
+  subject { create(:user) }
+  it { should validate_uniqueness_of(:email).ignoring_case_sensitivity }
+
   it { should validate_length_of(:username).is_at_least(3) }
 
   it { should validate_presence_of(:email) }
-  it { should validate_uniqueness_of(:email).ignoring_case_sensitivity }
+
   it { should allow_value('test@example.com').for(:email) }
   it { should_not allow_value('invalid-email').for(:email) }
 
