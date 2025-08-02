c2eb46901ec0414a070a8824ba73acb12e82c485
Fikir <fikirmrw@gmail.com>
2025-08-03 00:56:09 +0300

    Minor bug fixes

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,14 +1,12 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # This is where you specify the origin of your frontend application
-    # If the frontend is running on http://localhost:3000, use that.
-    # For deployment, you will change this to our actual domain.
-    origins "http://localhost:3000"
+    # Allow requests from your frontend's URL
+    origins "http://localhost:5173"
 
     resource "*",
       headers: :any,
-      methods: [ :get, :post, :put, :patch, :delete, :options, :head ],
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
       credentials: true
   end
 end
