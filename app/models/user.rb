eb9be74b5f98ed97a747c93983e6b99dcafbc9fa
Fikir <fikirmrw@gmail.com>
2025-08-02 16:45:10 +0300

    Configure models and add JWT service

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
+# app/models/user.rb
 class User < ApplicationRecord
+  has_secure_password
+  has_many :notes, dependent: :destroy
+
+  validates :username, presence: true, uniqueness: true, length: { minimum: 3 }
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, presence: true, length: { minimum: 6 }, on: :create
 end
